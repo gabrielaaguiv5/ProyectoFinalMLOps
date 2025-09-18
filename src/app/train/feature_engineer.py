3fb546796decb730be4563f521852deb2b26e55a
gabrielaaguiv5 <gabrielaaguiv@gmail.com>
2025-09-17 23:20:02 -0500

    Update Gabriela Aguilar

--- src/app/train/feature_engineer.py
+++ src/app/train/feature_engineer.py
@@ -5,3 +5,13 @@ import numpy as np
 class FeatureEngineer:
     def __init__(self, df):
         self.df = df
+    
+    def linea_tiempo(self):
+        if self.df is None:
+            raise ValueError("Llama primero a create_dataset().")
+        self.df.sort_values(["CustomerID", "InvoiceDate", "Description"], kind="mergesort", inplace=True)
+        return self.df
+    
+    def create_features(self):
+        self.df["Revenue"] = self.df["Quantity"] * self.df["UnitPrice"]
+        
